882b4f61270c4b4e3bdcdcd186ea022f4e8007df
LeaG588 <lg4112588@gmail.com>
2024-09-26 12:54:32 +0300

    Uploading the project to github

---
+++ src/app/modules/interfaces.ts
@@ -0,0 +1,22 @@
+
+
+
+export interface Receipt {
+    receiptNumber: number,
+    customer: Customer,
+    sum: number,
+    paymentMethods:string,
+    date:Date,
+    details:string
+}
+export interface Customer {
+    name: string,
+    number:string
+}
+export interface Expense {
+    date: Date,
+    sum:string,
+    amount:number,
+    paymentMethods:string,
+    details:string
+}
